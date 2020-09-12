fafb78bb83263677efcf69cc95465420ed63bee2
KennyJr <kenecsejr91@outlook.com>
2020-09-12 11:32:01 -0400

    Learning the basics about declarations, variables and experimenting with adding variables into strings.

---
+++ KotlinTutorial/app/src/main/java/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    println("Hello World")
+    println("This conversation is being recorded")
+
+    val salutation = "From Russia with Love \n- V. Putin"
+
+    println(salutation)
+
+    var madeUpTime = 25
+    madeUpTime++
+    madeUpTime += 5
+    print(madeUpTime)
+
+    val apples: Int = 6
+    val oranges: Int = 35
+    val fruit = apples + oranges
+    println("\n\n Putin wants ${apples + oranges} pieces of fruit.  \n\nallegedly. Please don't sue")
+
+    val weeks: Double = 130.0
+    val years: Double = weeks/52
+    println("\n\n$years")
+}
