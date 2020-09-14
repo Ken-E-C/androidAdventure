a325d9854f38668e04d1359591df2b96b153e7aa
KennyJr <kenecsejr91@outlook.com>
2020-09-14 06:41:41 -0400

    Some more stuff on string interpolation and datatype inference

--- KotlinTutorial/app/src/main/java/Main.kt
+++ KotlinTutorial/app/src/main/java/Main.kt
@@ -3,20 +3,26 @@ fun main(args: Array<String>) {
     println("This conversation is being recorded")
 
     val salutation = "From Russia with Love \n- V. Putin"
-
+    val kirkName = "James T Kirk"
     println(salutation)
 
     var madeUpTime = 25
     madeUpTime++
     madeUpTime += 5
-    print(madeUpTime)
+    print("Time you have left: $madeUpTime months")
 
-    val apples: Int = 6
-    val oranges: Int = 35
-    val fruit = apples + oranges
-    println("\n\n Putin wants ${apples + oranges} pieces of fruit.  \n\nallegedly. Please don't sue")
-
-    val weeks: Double = 130.0
-    val years: Double = weeks/52
+    val apples = 6
+    val oranges = 35
+    //example of String Interpolation
+    println("\n\nPutin wants ${apples + oranges} pieces of fruit.  \n\nallegedly. Please don't sue")
+    //val == let in Kotlin
+    val weeks = 130.0
+    val years = weeks/52
     println("\n\n$years")
+
+    val name = "Putin"
+    println("My name is " + name)
+    println("This is $kirkName")
+
+
 }
