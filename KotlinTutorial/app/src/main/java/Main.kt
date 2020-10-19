3a2f2cf9d716ebcc245ec5ec492ef13775946b0b
KennyJr <kenecsejr91@outlook.com>
2020-10-19 08:35:12 -0400

    Intro to inheritance

--- KotlinTutorial/app/src/main/java/Main.kt
+++ KotlinTutorial/app/src/main/java/Main.kt
@@ -1,45 +1,15 @@
 fun main(args: Array<String>) {
-    val ken = Player("Kenny")
-    ken.show()
+    val enemy = Enemy("test Enemy", 10, 3)
 
-    val louis = Player("Louis", 5)
-    louis.show()
+    enemy.takeDamage(4)
+    println(enemy)
 
-    val peter = Player("Peter", 0, 0, -100)
-    peter.show()
+    enemy.takeDamage(11)
+    println(enemy)
 
-    val axe = Weapon("axe",12)
-    peter.weapon = axe
-    axe.damageInflicted = 24
+    val uglyTroll = Troll("Ugly Troll")
+    println(uglyTroll)
+    uglyTroll.takeDamage(30)
+    println(uglyTroll)
 
-
-    ken.weapon = Weapon("Sword", 15)
-    louis.weapon = ken.weapon
-    ken.weapon = Weapon("Spear",30)
-
-    louis.show()
-    println(ken)
-
-    val redPotion = Loot("Red Potion", LootType.POTION, 7.50)
-    ken.addLoot(redPotion)
-    val chestArmor = Loot("+3 Chest Armor", LootType.ARMOR, 80.0)
-    ken.addLoot(chestArmor)
-    ken.showInventory()
-    ken.addLoot(Loot("Ring of Protection",LootType.RING, 40.25))
-    ken.addLoot(Loot("Invisibility potion", LootType.POTION, 35.95))
-    ken.showInventory()
-
-    if (ken.dropLoot(redPotion)) {
-        ken.showInventory()
-    }
-
-    val bluePotion = Loot("Blue Potion", LootType.POTION, 10.0)
-
-    ken.dropLoot(bluePotion)
-
-    if (ken.dropLoot("Invisibility potion")) {
-        ken.showInventory()
-    } else {
-        println("You don't have this item")
-    }
 }
